3e501637bc5632fbb608a8e3eea915b93ad8821d
manne ohrstrom <manne@shotgunsoftware.com>
2013-04-21 10:22:08 +0100

    Added 0.13 related items to config.

--- after_project_create.py
+++ after_project_create.py
@@ -16,30 +16,10 @@ the following keyword arguments:
 
 """
 
-# list of tank types to create for this starter configuration
-REQUIRED_TYPES = ["Maya Anim",
-                  "Maya Lighting",
-                  "Maya Model",
-                  "Maya Rig",
-                  "Maya Scene",
-                  "Motion Builder FBX",
-                  "Photoshop Image",
-                  "3dsmax Scene",
-                  "Nuke Script",
-                  "Rendered Image",
-                  "Review Quicktime",
-                  "Review Sequence",
-                  "Stereo Rendered Image",
-                  "Diffuse Texture",
-                  "Specular Texture"]
-
 def create(sg, project_id, log, **kwargs):
-    project = {"type": "Project", "id": project_id}
-    entity_name = "TankType"
-    cur_entities = sg.find(entity_name, [["project", "is", project]], ["code"])
-    existing_types = [x["code"] for x in cur_entities]
-    needed_types = set(REQUIRED_TYPES) - set(existing_types)
-    for tank_type in needed_types:
-        log.info("Creating tank type '%s'" % tank_type)
-        sg.create(entity_name, {"code": tank_type, "project": project})
+    """
+    Insert post-project code here
+    """
+    # the default config does not require any post-session stuff.
+    pass
 
