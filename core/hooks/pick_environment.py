29fa75b8bb339215356eef076224f13443537a47
manne ohrstrom <manne@shotgunsoftware.com>
2013-05-02 16:38:05 +0100

    added a step and a shot based environment.

--- core/hooks/pick_environment.py
+++ core/hooks/pick_environment.py
@@ -3,7 +3,6 @@ Copyright (c) 2012 Shotgun Software, Inc
 ----------------------------------------------------
 
 Hook which chooses an environment file to use based on the current context.
-This file is almost always overridden by a standard config.
 
 """
 
@@ -22,12 +21,20 @@ class PickEnvironment(Hook):
             # don't know how to handle this case.
             return None
         
-        if context.project and context.entity is None:
-            # project-only context
+        if context.entity is None:
+            # we have a project but not an entity
             return "project"
-        elif context.entity["type"] == "Shot":
-            return "shot"
-        elif context.entity["type"] == "Asset":
-            return "asset"
+        
+        if context.entity and context.step is None:
+            # we have an entity but no step!
+            # this is convered by a single environment
+            return "shot_and_asset"
+            
+        if context.entity and context.step:
+            # we have a step and an entity
+            if context.entity["type"] == "Shot":
+                return "shot_step"
+            if context.entity["type"] == "Asset":
+                return "asset_step"            
 
         return None
