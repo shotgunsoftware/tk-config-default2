994a423dd52d482f39ff45b08fbf2dc46383f9b5
Mike Brainerd <mbrainerd@d2.com>
2017-11-27 19:05:57 -0800

    initial pass

--- core/hooks/pick_environment.py
+++ core/hooks/pick_environment.py
@@ -12,10 +12,10 @@
 Hook which chooses an environment file to use based on the current context.
 
 """
+import sgtk
+HookBaseClass = sgtk.get_hook_baseclass()
 
-from tank import Hook
-
-class PickEnvironment(Hook):
+class PickEnvironment(HookBaseClass):
 
     def execute(self, context, **kwargs):
         """
@@ -37,18 +37,20 @@ class PickEnvironment(Hook):
 
         if context.entity and context.step is None:
             # we have an entity but no step!
-            if context.entity["type"] == "Shot":
-                return "shot"
-            if context.entity["type"] == "Asset":
-                return "asset"
-            if context.entity["type"] == "Sequence":
-                return "sequence"
+            return "project"
 
         if context.entity and context.step:
             # we have a step and an entity
+            if context.entity["type"] == "Sequence":
+                return "sequence"
             if context.entity["type"] == "Shot":
-                return "shot_step"
+                return "shot"
             if context.entity["type"] == "Asset":
-                return "asset_step"
+                entities_by_type = dict([(x["type"], x) for x in context.additional_entities])
+                if "Shot" in entities_by_type:
+                    return "shot_asset"
+                if "Sequence" in entities_by_type:
+                    return "sequence_asset"
+                return "asset"
 
         return None
