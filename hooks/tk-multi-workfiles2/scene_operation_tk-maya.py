eaee86be1509b71294fa1bd768c385b1f6aded54
SaiAparna Ramamurthy <raparna@d2.com>
2018-01-31 10:58:31 +0530

    Addressed code review comments.

--- hooks/tk-multi-workfiles2/scene_operation_tk-maya.py
+++ hooks/tk-multi-workfiles2/scene_operation_tk-maya.py
@@ -71,10 +71,15 @@ class SceneOperation(HookClass):
             cmds.file(new=True, force=True)
             cmds.file(file_path, open=True, force=True)
 
-            # getting fields from file path because context.as_template_fields() doesn't contain {name}, {version}
+            # getting fields from file path because
+            # context.as_template_fields() doesn't contain {name}, {version}
             file_template = context.sgtk.template_from_path(file_path)
-            fields = file_template.get_fields(file_path)
+            if not file_template:
+                self.parent.logger.warning("Current file path doesn't conform to Shotgun template. "
+                                           "Not setting render defaults.")
+                return
 
+            fields = file_template.get_fields(file_path)
             render_temp = self.get_render_template(context)
             frame_sq_key = context.sgtk.template_keys['SEQ']    # Can 'SEQ' change?
 
@@ -83,12 +88,14 @@ class SceneOperation(HookClass):
             try:
                 fields.update({"width": indiapipeline_prefs["show_resolution"]["width"],
                                "height": indiapipeline_prefs["show_resolution"]["height"],
-                               "output": "LAYERPLACEHOLDER"})   # output is alphanumeric; replace with token <Layer>
+                               "output": "LAYERPLACEHOLDER"})   # output is alphanumeric
+                                                                # replace with token <Layer>
             except KeyError as ke:
-                # logging?
-                print "Unable to find {} in indiapipeline preferences. Not setting render defaults.".format(ke)
+                self.parent.logger.warning("Unable to find {} in indiapipeline preferences. "
+                                           "Not setting render defaults.".format(ke))
             else:
-                fields.pop("extension")                         # remove ma as extension to apply default img ext
+                fields.pop("extension")                         # remove ma as extension
+                                                                # to apply default img ext
                 render_path = render_temp.apply_fields(fields).replace("LAYERPLACEHOLDER", "<Layer>")
                 self.set_render_defaults(fields, render_path, frame_sq_key)
                 self.set_vray_settings(fields, render_path, frame_sq_key)
@@ -142,27 +149,38 @@ class SceneOperation(HookClass):
             return True
 
     def get_render_template(self, context):
-        fields = context.as_template_fields()
-        templates = context.sgtk.templates
-        entity_type = context.entity["type"]
+        """
+        Get template for path where the render should be saved.
 
-        if entity_type == "Asset":
-            if "Shot" in fields:
-                render_temp = templates["tk-maya_shot_asset_work_render"]
-            elif "Sequence" in fields:
-                render_temp = templates["tk-maya_sequence_asset_work_render"]
-            else:
-                render_temp = templates["tk-maya_project_asset_work_render"]
-        elif entity_type == "Shot":
-            render_temp = templates["tk-maya_shot_work_render"]
-        elif entity_type == "Sequence":
-            render_temp = templates["tk-maya_sequence_work_render"]
-        else:  # elif type == "Project": (?)
-            render_temp = templates["tk-maya_project_work_render"]
+        :param context: Context
+                        The context of current operation.
+
+        :return:        Template
+                        Shotgun template for render path according to current context.
+        """
+        templates = context.sgtk.templates
+
+        template_exp = "{engine_name}_{env_name}_work_render"
+        template_name = self.parent.resolve_setting_expression(template_exp)
+        render_temp = templates.get(template_name)
 
         return render_temp
 
     def set_render_defaults(self, fields, render_path, frame_sq_key):
+        """
+        Set render file name and path, image format and image resolution for default Maya renderers.
+
+        :param fields:          dict
+                                Template fields used to create the final render path
+
+        :param render_path:     String
+                                Complete path to the rendered image
+
+        :param frame_sq_key:    SequenceKey
+                                Object containing the number format for the frame sequence
+
+        :returns:               None
+        """
         # set resolution
         self.unlock_and_set_attr("defaultResolution.aspectLock", False, lock=True)
         self.unlock_and_set_attr("defaultResolution.width", fields["width"], lock=True)
@@ -175,16 +193,33 @@ class SceneOperation(HookClass):
         mel.eval("setMayaSoftwareImageFormat {}".format(ext))
 
         # set file prefix
-        self.unlock_and_set_attr("defaultRenderGlobals.imageFilePrefix", prefix, type="string", lock=True)
+        self.unlock_and_set_attr("defaultRenderGlobals.imageFilePrefix", prefix,
+                                 attribute_type="string", lock=True)
 
         # ensure frame/animation ext = name.#.ext
         self.set_enum_attr("defaultRenderGlobals.outFormatControl", "As Output Format", lock=True)
         self.unlock_and_set_attr("defaultRenderGlobals.animation", True, lock=True)
         self.unlock_and_set_attr("defaultRenderGlobals.putFrameBeforeExt", True, lock=True)
-        self.unlock_and_set_attr("defaultRenderGlobals.extensionPadding", int(frame_sq_key.format_spec), lock=True)
+        self.unlock_and_set_attr("defaultRenderGlobals.extensionPadding",
+                                 int(frame_sq_key.format_spec), lock=True)
         self.set_enum_attr("defaultRenderGlobals.periodInExt", "Period in Extension", lock=True)
 
     def set_vray_settings(self, fields, render_path, frame_sq_key):
+        """
+        Set render file name and path, image format and image resolution
+        for any vray nodes in the maya file.
+
+        :param fields:          dict
+                                Template fields used to create the final render path
+
+        :param render_path:     String
+                                Complete path to the rendered image
+
+        :param frame_sq_key:    SequenceKey
+                                Object containing the number format for the frame sequence
+
+        :returns:               None
+        """
         if not cmds.pluginInfo("vrayformaya", query=True, loaded=True):
             return
         vray_nodes = cmds.ls(type="VRaySettingsNode")
@@ -198,19 +233,37 @@ class SceneOperation(HookClass):
 
             prefix, ext = self.split_prefix_ext(render_path, frame_sq_key)
 
-            self.unlock_and_set_attr("{}.imageFormatStr".format(node), ext, type="string", lock=True)
-            self.unlock_and_set_attr("{}.fileNamePrefix".format(node), prefix, type="string", lock=True)
+            self.unlock_and_set_attr("{}.imageFormatStr".format(node), ext,
+                                     attribute_type="string", lock=True)
+            self.unlock_and_set_attr("{}.fileNamePrefix".format(node), prefix,
+                                     attribute_type="string", lock=True)
 
             # ensure frame/animation ext = name.#.ext
             self.set_enum_attr("{}.animType".format(node), "Standard", lock=True)
-            self.unlock_and_set_attr("{}.fileNamePadding".format(node), int(frame_sq_key.format_spec), lock=True)
+            self.unlock_and_set_attr("{}.fileNamePadding".format(node),
+                                     int(frame_sq_key.format_spec), lock=True)
 
     # move to a maya utils location?
-    def set_enum_attr(self, attribute_full, value, lock=False):
+    @staticmethod
+    def set_enum_attr(attribute_full, value, lock=False):
+        """
+        Set enum value for a maya node attribute using its string representation.
+
+        :param attribute_full:  String
+                                Full name of the attribute of format <node_name>.<attribute_name>
+
+        :param value:           String
+                                Value that should be assigned to the attribute passed
+
+        :param lock:            Boolean
+                                Whether the attribute should be locked after it is set.
+
+        :return:                None
+        """
         node, attribute_short = attribute_full.split('.', 1)
         enum_string = cmds.attributeQuery(attribute_short, node=node, listEnum=True)[0]
         enum_list = enum_string.split(':')
-        enum_dict = {}; index=0
+        enum_dict = {}; index = 0
         for enum_item in enum_list:
             if '=' in enum_item:
                 index = enum_item.split('=')[1]
@@ -219,16 +272,48 @@ class SceneOperation(HookClass):
                 enum_dict[enum_item] = index
             index += 1
 
-        self.unlock_and_set_attr(attribute_full, enum_dict[value], lock=lock)
+        SceneOperation.unlock_and_set_attr(attribute_full, enum_dict[value], lock=lock)
 
-    def unlock_and_set_attr(self, attribute, value, type=None, lock=False):
+    @staticmethod
+    def unlock_and_set_attr(attribute, value, attribute_type=None, lock=False):
+        """
+        Utility maya method to unlock a node attribute and set its value.
+
+        :param attribute:       String
+                                Full name of the attribute of format <node_name>.<attribute_name>
+
+        :param value:           Value that should be assigned to the attribute passed
+
+        :param attribute_type:  String
+                                Type of passed value (according to Maya)
+                                Needs to be specified if non-numeric
+
+        :param lock:            Boolean
+                                Whether the attribute should be locked after it is set.
+
+        :return:                None
+        """
         cmds.setAttr(attribute, lock=False)
-        if type:
-            cmds.setAttr(attribute, value, type=type, lock=lock)
+        if attribute_type:
+            cmds.setAttr(attribute, value, type=attribute_type, lock=lock)
         else:
             cmds.setAttr(attribute, value, lock=lock)
 
-    def split_prefix_ext(self, render_path, frame_sq_key):
+    @staticmethod
+    def split_prefix_ext(render_path, frame_sq_key):
+        """
+        Utility method to split the render path into a prefix
+        that can be passed to maya render settings and image extension.
+
+        :param render_path:     String
+                                Path where the renders should be saved.
+
+        :param frame_sq_key:    SequenceKey
+                                Object containing the number format for the frame sequence
+
+        :return:                Tuple (String, String)
+                                File name prefix and extension.
+        """
         prefix, ext = os.path.splitext(render_path)
         ext = ext.lstrip('.')
         prefix = prefix.replace("." + frame_sq_key.default, "")
