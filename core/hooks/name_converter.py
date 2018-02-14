da999663923f7c3e8941ec9c7608a24d0344db70
Garvit Verma <gverma@d2.com>
2018-02-14 15:02:03 +0530

    localized _validate method so that we have validations that don't break with invalid choices

--- core/hooks/name_converter.py
+++ core/hooks/name_converter.py
@@ -16,6 +16,7 @@ import os
 import copy
 
 import sgtk
+from tank.templatekey import StringKey
 
 HookBaseClass = sgtk.get_hook_baseclass()
 
@@ -34,8 +35,84 @@ class TemplateKeyCustom(HookBaseClass):
                                     such as a subset calculation
         :returns: Bool
         """
+        # since use-case of name_converter is only for modifying StringKey, copying the validations that StringKey does.
+        if isinstance(self.parent, StringKey):
+            return self._validate(value, validate_transforms)
+        else:
+            return self.parent._validate(value, validate_transforms)
+
+    def _validate(self, value, validate_transforms, **kwargs):
+        """
+        Test if a value is valid for this key
+
+        :param value: Value to test
+        :param validate_transforms: Bool to enable validation of transforms,
+                                    such as a subset calculation
+        :returns: Bool
+        """
+        u_value = value
+        if not isinstance(u_value, unicode):
+            # handle non-ascii characters correctly by
+            # decoding to unicode assuming utf-8 encoding
+            u_value = value.decode("utf-8")
+
+        if self.parent._filter_regex_u:
+            # first check our std filters. These filters are negated
+            # so here we are checking that there are occurances of
+            # that pattern in the string
+            if self.parent._filter_regex_u.search(u_value):
+                self.parent._last_error = "%s Illegal value '%s' does not fit filter_by '%s'" % (self.parent, value,
+                                                                                                 self.parent.filter_by)
+                return False
+
+        elif self.parent._custom_regex_u:
+            # check for any user specified regexes
+            if self.parent._custom_regex_u.match(u_value) is None:
+                self.parent._last_error = "%s Illegal value '%s' does not fit filter_by '%s'" % (self.parent, value,
+                                                                                                 self.parent.filter_by)
+                return False
+
+        # check subset regex
+        if self.parent._subset_regex and validate_transforms:
+            regex_match = self.parent._subset_regex.match(u_value)
+            if regex_match is None:
+                self.parent._last_error = "%s Illegal value '%s' does not fit " \
+                                          "subset expression '%s'" % (self.parent, value, self.parent.subset)
+                return False
+
+            # validate that the formatting can be applied to the input value
+            if self.parent._subset_format:
+                try:
+                    # perform the formatting in unicode space to cover all cases
+                    self.parent._subset_format.decode("utf-8").format(*regex_match.groups())
+                except Exception as e:
+                    self.parent._last_error = "%s Illegal value '%s' does not fit subset '%s' with format '%s': %s" % (
+                        self.parent,
+                        value,
+                        self.parent.subset,
+                        self.parent.subset_format,
+                        e
+                    )
+                    return False
+
+        str_value = value if isinstance(value, basestring) else str(value)
+
+        # We are not case sensitive
+        if str_value.lower() in [str(x).lower() for x in self.parent.exclusions]:
+            self.parent._last_error = "%s Illegal value: %s is forbidden for this key." % (self.parent, value)
+            return False
+
         # to avoid the validation to fail, if the value is not defined in choices!!
-        # return self.parent._validate(value, validate_transforms)
+        # if value is not None and self.choices:
+        #     if str_value.lower() not in [str(x).lower() for x in self.choices]:
+        #         self._last_error = "%s Illegal value: '%s' not in choices: %s" % (self, value, str(self.choices))
+        #         return False
+
+        if self.parent.length is not None and len(str_value) != self.parent.length:
+            self.parent._last_error = ("%s Illegal value: '%s' does not have a length of "
+                                       "%d characters." % (self.parent, value, self.parent.length))
+            return False
+
         return True
 
     @staticmethod
@@ -58,22 +135,23 @@ class TemplateKeyCustom(HookBaseClass):
         :returns: The translated value.
         """
 
-        choices = copy.deepcopy(self.parent.labelled_choices)
-        edit = choices.pop(EDIT_TYPE)
+        if isinstance(self.parent, StringKey):
+            choices = copy.deepcopy(self.parent.labelled_choices)
+            edit = choices.pop(EDIT_TYPE)
 
-        # removed "pad" type edit, since that is already taken care of by 'format_spec'
+            # removed "pad" type edit, since that is already taken care of by 'format_spec'
 
-        if edit in VALID_EDITS:
-            if edit == "replace":
-                relevant_replaces = [replace for replace in choices if replace in str_value]
-                for replace in relevant_replaces:
-                    str_value = str_value.replace(replace, choices[replace])
-            elif edit == "lower_case":
-                str_value = str_value.lower()
-            elif edit == "upper_case":
-                str_value = str_value.upper()
-            elif edit == "underscore_to_camelcase":
-                str_value = self._underscore_to_camelcase(str_value)
+            if edit in VALID_EDITS:
+                if edit == "replace":
+                    relevant_replaces = [replace for replace in choices if replace in str_value]
+                    for replace in relevant_replaces:
+                        str_value = str_value.replace(replace, choices[replace])
+                elif edit == "lower_case":
+                    str_value = str_value.lower()
+                elif edit == "upper_case":
+                    str_value = str_value.upper()
+                elif edit == "underscore_to_camelcase":
+                    str_value = self._underscore_to_camelcase(str_value)
 
         return str_value
 
