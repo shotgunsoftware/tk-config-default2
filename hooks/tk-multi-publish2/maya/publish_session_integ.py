854153730116fd537e91dc89d57bf8de7ed76736
Mike Brainerd <mbrainerd@d2.com>
2018-07-30 16:22:44 -0700

    re-merged maya/publish_files and publish_session.

--- hooks/tk-multi-publish2/maya/publish_session_integ.py
+++ hooks/tk-multi-publish2/maya/publish_session_integ.py
@@ -14,6 +14,10 @@ import maya.mel as mel
 import sgtk
 from sgtk.platform.qt import QtGui
 
+from dd.runtime import api
+api.load('frangetools')
+import frangetools
+
 HookBaseClass = sgtk.get_hook_baseclass()
 
 GROUP_NODES = ['WORLDSCALE',
@@ -45,6 +49,49 @@ class MayaPublishSessionDDIntegValidationPlugin(HookBaseClass):
         return desc + "<br><br>" + """
         Validation checks before a file is published.
         """
+
+
+    def _build_dict(self, seq, key):
+        """
+        Creating a dictionary based on a key.
+
+        :param seq: list of dictionaries
+        :param key: dictionary key from which to create the dictionary
+        :return: dict with information arranged based on that particular key
+        """
+        return dict((d[key], dict(d, index=index)) for (index, d) in enumerate(seq))
+
+
+    def _framerange_of_sequence(self, item):
+        """
+        Since users have the option to render only a subset of frames,
+        adding validation to check if the full frame range is being published.
+
+        :param item: Item to process
+        :return: True if yes false otherwise
+        """
+        lss_path = item.properties['path']
+        lss_data = frangetools.getSequence(lss_path)
+
+        info_by_path = self._build_dict(lss_data, key="path")
+        missing_frames = info_by_path.get(lss_path)['missing_frames']
+
+        if missing_frames:
+            self.logger.error("Incomplete playblast! All the frames are not the playblast.")
+            return False
+        else:
+            # If there are no missing frames, checking if the start and end frames match with playblast settings.
+            # This is being directly checked with playblast settings in the scene since
+            # _sync_frame_range_with_shotgun() will ensure playblast frame range is synced with shotgun
+            import pymel.core as pm
+            playback_start = pm.playbackOptions(q=True, minTime=True)
+            playback_end = pm.playbackOptions(q=True, maxTime=True)
+            collected_playblast_firstframe = info_by_path.get(lss_path)['frame_range'][0]
+            collected_playblast_lastframe = info_by_path.get(lss_path)['frame_range'][1]
+            if (collected_playblast_firstframe != playback_start) or (collected_playblast_lastframe != playback_end):
+                self.logger.error("Incomplete playblast! All the frames are not in the playblast.")
+                return False
+        return True
 
 
     def _sync_frame_range_with_shotgun(self, item):
@@ -342,6 +389,11 @@ class MayaPublishSessionDDIntegValidationPlugin(HookBaseClass):
                         self._sync_frame_range_with_shotgun(item)
                 cam = self._camera_naming() and self._connected_image_plane()
                 status = nodes and cam and status
+
+        # Checks for the scene file, i.e if the item is not a sequence or a cache file
+        if item.properties.get('is_sequence', False):
+            sequences = self._framerange_of_sequence(item)
+            status = sequences and status
 
         if not status:
             return status
