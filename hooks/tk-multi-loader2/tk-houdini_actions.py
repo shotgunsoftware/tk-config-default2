f9b80184ad33670a00123218555d967a0ac9165f
Jesin Roy <jesinroy@gmail.com>
2018-10-30 10:04:33 +0530

    Loading image seq into a camera node instead of File COP (#82)

--- hooks/tk-multi-loader2/tk-houdini_actions.py
+++ hooks/tk-multi-loader2/tk-houdini_actions.py
@@ -15,10 +15,81 @@ import os
 import re
 import sgtk
 import sys
+import hou
 
 HookBaseClass = sgtk.get_hook_baseclass()
 
 class CustomHoudiniActions(HookBaseClass):
+
+    def generate_actions(self, sg_publish_data, actions, ui_area):
+        """
+        Returns a list of action instances for a particular publish.
+        This method is called each time a user clicks a publish somewhere in the UI.
+        The data returned from this hook will be used to populate the actions menu for a publish.
+
+        The mapping between Publish types and actions are kept in a different place
+        (in the configuration) so at the point when this hook is called, the loader app
+        has already established *which* actions are appropriate for this object.
+
+        The hook should return at least one action for each item passed in via the
+        actions parameter.
+
+        This method needs to return detailed data for those actions, in the form of a list
+        of dictionaries, each with name, params, caption and description keys.
+
+        Because you are operating on a particular publish, you may tailor the output
+        (caption, tooltip etc) to contain custom information suitable for this publish.
+
+        The ui_area parameter is a string and indicates where the publish is to be shown.
+        - If it will be shown in the main browsing area, "main" is passed.
+        - If it will be shown in the details area, "details" is passed.
+        - If it will be shown in the history area, "history" is passed.
+
+        Please note that it is perfectly possible to create more than one action "instance" for
+        an action! You can for example do scene introspection - if the action passed in
+        is "character_attachment" you may for example scan the scene, figure out all the nodes
+        where this object can be attached and return a list of action instances:
+        "attach to left hand", "attach to right hand" etc. In this case, when more than
+        one object is returned for an action, use the params key to pass additional
+        data into the run_action hook.
+
+        :param sg_publish_data: Shotgun data dictionary with all the standard publish fields.
+        :param actions: List of action strings which have been defined in the app configuration.
+        :param ui_area: String denoting the UI Area (see above).
+        :returns List of dictionaries, each with keys name, params, caption and description
+        """
+
+        # get the existing action instances
+        action_instances = super(CustomHoudiniActions, self).generate_actions(sg_publish_data, actions, ui_area)
+
+        if "image_plane" in actions:
+            action_instances.append({
+                "name": "image_plane",
+                "params": None,
+                "caption": "Create Camera Image Plane",
+                "description": "Creates image plane for the selected camera or adds a new camera node with image plane set."
+            })
+
+        return action_instances
+
+    ##############################################################################################################
+
+    def execute_action(self, name, params, sg_publish_data):
+        """
+        Execute a given action. The data sent to this be method will
+        represent one of the actions enumerated by the generate_actions method.
+
+        :param name: Action name string representing one of the items returned by generate_actions.
+        :param params: Params data, as specified by generate_actions.
+        :param sg_publish_data: Shotgun data dictionary with all the standard publish fields.
+        :returns: No return value expected.
+        """
+
+        # call the actions from super
+        super(CustomHoudiniActions, self).execute_action(name, params, sg_publish_data)
+
+        if name == "image_plane":
+            self._create_image_plane(sg_publish_data)
 
     ##############################################################################################################
     def _import(self, path, sg_publish_data):
@@ -29,7 +100,6 @@ class CustomHoudiniActions(HookBaseClass):
 
         """
 
-        import hou
         app = self.parent
 
         name = sg_publish_data.get("name")
@@ -82,3 +152,49 @@ class CustomHoudiniActions(HookBaseClass):
         parent_module._show_node(node)
 
     ##############################################################################################################
+
+    def _create_image_plane(self, sg_publish_data):
+        """
+        Adds an image plane for the selected camera or create a new camera node
+
+        :param sg_publish_data: Shotgun data dictionary with all the standard
+            publish fields.
+        """
+
+        app = self.parent
+
+        name = sg_publish_data.get("name")
+        path = self.get_publish_path(sg_publish_data)
+        parent_module = sys.modules[HookBaseClass.__module__]
+
+        # houdini doesn't like UNC paths.
+        path = path.replace("\\", "/")
+
+        obj_context = parent_module._get_current_context("/obj")
+
+        selected_nodes = hou.selectedNodes()
+        if selected_nodes:
+            node_type = hou.nodeType(selected_nodes[0].path()).name()
+            if len(selected_nodes) > 1:
+                hou.ui.displayMessage("Please select only one camera node.")
+            elif node_type != "cam":
+                hou.ui.displayMessage("Please select a camera node.")
+            else:
+                camera_node = selected_nodes[0]
+                camera_node.parm("vm_background").set(path)
+                node_name = hou.nodeType(camera_node.path()).name()
+                app.log_debug(
+                    "Adding background image to %s node: %s\n  path: '%s' " %
+                    (node_name, camera_node.path(), path)
+                )
+        else:
+            camera_node = obj_context.createNode("cam", name)
+            camera_node.parm("vm_background").set(path)
+
+            node_name = hou.nodeType(camera_node.path()).name()
+            app.log_debug(
+                "Creating %s node: %s\n  path: '%s' " %
+                (node_name, camera_node.path(), path)
+            )
+
+            parent_module._show_node(camera_node)
