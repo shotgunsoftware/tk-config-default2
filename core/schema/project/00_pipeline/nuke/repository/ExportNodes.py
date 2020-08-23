5afb662b6d156e7ac4c76f2888c99a8db2329157
gillesvink <gilles@meneervink.nl>
2020-08-23 23:10:22 +0200

    Added error functionality as well as comments in the code.

--- core/schema/project/00_pipeline/nuke/repository/ExportNodes.py
+++ core/schema/project/00_pipeline/nuke/repository/ExportNodes.py
@@ -4,12 +4,16 @@ import os
 
 
 def main():
-
+    # Getting current location
     dirname = os.path.dirname(os.path.abspath(__file__))
+
+    # Saving selected nodes for export
     selection = nuke.selectedNodes()
     if selection == []:
         nuke.message("There are no nodes selected.")
     else:
+
+        # Creating Template creation menu
         class TemplateCreation(nukescripts.PythonPanel):
             def __init__(self, node):
 
@@ -20,24 +24,45 @@ def main():
                 for k in (self.typeKnob, self.elementKnob):
                     self.addKnob(k)
 
+        # Activating menu
         tc = TemplateCreation(selection)
         if tc.showModalDialog():
+            # Getting values from menu
             exptype = tc.typeKnob.value()
             expname = tc.elementKnob.value()
 
             if expname == '':
                 nuke.message('There was no name specified.')
             else:
-                nuke.message("Export as:" + exptype + " " + expname)
+                # Template export settings
                 if exptype == 'Template':
+                    # Creating export paths
                     expfile = expname + '.nk'
                     exploc = os.path.join(dirname, "project_repo","Templates", expfile)
+
+                    # Fixing windows slashes functionality
                     exploc_win = exploc.replace(os.sep, '/')
-                    nuke.nodeCopy(exploc_win)
-                    nuke.message("Exporting " + expname + " as template was successful.")
+
+                    # Exporting as Nuke file
+                    try:
+                        nuke.nodeCopy(exploc_win)
+                        # Giving artist success message
+                        nuke.message("Exporting " + expname + " as template was successful.")
+                    except:
+                        nuke.message("An exception occurred")
+                # Node export settings
                 if exptype == 'Node':
+                    # Creating export paths
                     expfile = expname + '.nk'
                     exploc = os.path.join(dirname, "project_repo", "Nodes", expfile)
+
+                    # Fixing windows slashes functionality
                     exploc_win = exploc.replace(os.sep, '/')
-                    nuke.nodeCopy(exploc_win)
-                    nuke.message("Exporting " + expname + " as node was successful.")
+
+                    # Exporting as Nuke file
+                    try:
+                        nuke.nodeCopy(exploc_win)
+                        # Giving artist success message
+                        nuke.message("Exporting " + expname + " as node was successful.")
+                    except:
+                        nuke.message("An exception occurred")
