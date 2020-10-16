7c889425df02194e3990392bce81e33cf9afdf21
rthompson <rthompson@ssvfx.ie>
2020-10-16 10:35:22 +0100

    WIP

--- hooks/tk-multi-loader2/tk-houdini_actions.py
+++ hooks/tk-multi-loader2/tk-houdini_actions.py
@@ -1,15 +1,15 @@
 # Copyright (c) 2015 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 """
-Hook that loads defines all the available actions, broken down by publish type. 
+Hook that loads defines all the available actions, broken down by publish type.
 """
 import os
 import re
@@ -17,74 +17,85 @@ import sgtk
 
 HookBaseClass = sgtk.get_hook_baseclass()
 
+
 class HoudiniActions(HookBaseClass):
-    
+
     ##############################################################################################################
-    # public interface - to be overridden by deriving classes 
-    
+    # public interface - to be overridden by deriving classes
+
     def generate_actions(self, sg_publish_data, actions, ui_area):
         """
         Returns a list of action instances for a particular publish.
         This method is called each time a user clicks a publish somewhere in the UI.
         The data returned from this hook will be used to populate the actions menu for a publish.
-    
+
         The mapping between Publish types and actions are kept in a different place
         (in the configuration) so at the point when this hook is called, the loader app
         has already established *which* actions are appropriate for this object.
-        
-        The hook should return at least one action for each item passed in via the 
+
+        The hook should return at least one action for each item passed in via the
         actions parameter.
-        
+
         This method needs to return detailed data for those actions, in the form of a list
         of dictionaries, each with name, params, caption and description keys.
-        
-        Because you are operating on a particular publish, you may tailor the output 
+
+        Because you are operating on a particular publish, you may tailor the output
         (caption, tooltip etc) to contain custom information suitable for this publish.
-        
-        The ui_area parameter is a string and indicates where the publish is to be shown. 
-        - If it will be shown in the main browsing area, "main" is passed. 
+
+        The ui_area parameter is a string and indicates where the publish is to be shown.
+        - If it will be shown in the main browsing area, "main" is passed.
         - If it will be shown in the details area, "details" is passed.
-        - If it will be shown in the history area, "history" is passed. 
-        
-        Please note that it is perfectly possible to create more than one action "instance" for 
-        an action! You can for example do scene introspection - if the action passed in 
+        - If it will be shown in the history area, "history" is passed.
+
+        Please note that it is perfectly possible to create more than one action "instance" for
+        an action! You can for example do scene introspection - if the action passed in
         is "character_attachment" you may for example scan the scene, figure out all the nodes
         where this object can be attached and return a list of action instances:
-        "attach to left hand", "attach to right hand" etc. In this case, when more than 
-        one object is returned for an action, use the params key to pass additional 
+        "attach to left hand", "attach to right hand" etc. In this case, when more than
+        one object is returned for an action, use the params key to pass additional
         data into the run_action hook.
-        
+
         :param sg_publish_data: Shotgun data dictionary with all the standard publish fields.
         :param actions: List of action strings which have been defined in the app configuration.
         :param ui_area: String denoting the UI Area (see above).
         :returns List of dictionaries, each with keys name, params, caption and description
         """
         app = self.parent
-        app.log_debug("Generate actions called for UI element %s. "
-                      "Actions: %s. Publish Data: %s" % (ui_area, actions, sg_publish_data))
-        
+        app.log_debug(
+            "Generate actions called for UI element %s. "
+            "Actions: %s. Publish Data: %s" % (ui_area, actions, sg_publish_data)
+        )
+
         action_instances = []
-        
+
         if "merge" in actions:
-            action_instances.append( {"name": "merge", 
-                                      "params": None,
-                                      "caption": "Merge", 
-                                      "description": "This will merge the item into the scene."} )        
+            action_instances.append(
+                {
+                    "name": "merge",
+                    "params": None,
+                    "caption": "Merge",
+                    "description": "This will merge the item into the scene.",
+                }
+            )
         if "import" in actions:
-            action_instances.append({
-                "name": "import",
-                "params": None,
-                "caption": "Import",
-                "description": "Import the Alembic cache file into a geometry network.",
-            })
+            action_instances.append(
+                {
+                    "name": "import",
+                    "params": None,
+                    "caption": "Import",
+                    "description": "Import the Alembic cache file into a geometry network.",
+                }
+            )
 
         if "file_cop" in actions:
-            action_instances.append({
-                "name": "file_cop",
-                "params": None,
-                "caption": "File COP",
-                "description": "Load an image or image sequence via File COP.",
-            })
+            action_instances.append(
+                {
+                    "name": "file_cop",
+                    "params": None,
+                    "caption": "File COP",
+                    "description": "Load an image or image sequence via File COP.",
+                }
+            )
 
         return action_instances
 
@@ -123,19 +134,21 @@ class HoudiniActions(HookBaseClass):
         """
         Execute a given action. The data sent to this be method will
         represent one of the actions enumerated by the generate_actions method.
-        
+
         :param name: Action name string representing one of the items returned by generate_actions.
         :param params: Params data, as specified by generate_actions.
         :param sg_publish_data: Shotgun data dictionary with all the standard publish fields.
         :returns: No return value expected.
         """
         app = self.parent
-        app.log_debug("Execute action called for action %s. "
-                      "Parameters: %s. Publish Data: %s" % (name, params, sg_publish_data))
-        
+        app.log_debug(
+            "Execute action called for action %s. "
+            "Parameters: %s. Publish Data: %s" % (name, params, sg_publish_data)
+        )
+
         # resolve path
         path = self.get_publish_path(sg_publish_data)
-        
+
         if name == "merge":
             self._merge(path, sg_publish_data)
 
@@ -147,12 +160,12 @@ class HoudiniActions(HookBaseClass):
 
     ##############################################################################################################
     # helper methods which can be subclassed in custom hooks to fine tune the behaviour of things
-    
+
     def _merge(self, path, sg_publish_data):
         """
         Merge a published hip file into the working hip file with
         the default settings Houdini would use if you did it in the UI.
-        
+
         :param path: Path to file.
         :param sg_publish_data: Shotgun data dictionary with all the standard publish fields.
         """
@@ -160,9 +173,9 @@ class HoudiniActions(HookBaseClass):
 
         if not os.path.exists(path):
             raise Exception("File not found on disk - '%s'" % path)
-        
+
         # use the default settings, which tries to merge all nodes
-        # and is conservative about overwriting and errors                
+        # and is conservative about overwriting and errors
         #
         # NOTE: We're ensuring that the path uses forward-slash separators
         # since some hearly H17 builds had major issues with backslashes on
@@ -174,17 +187,17 @@ class HoudiniActions(HookBaseClass):
             ignore_load_warnings=False,
         )
 
-
     ##############################################################################################################
     def _import(self, path, sg_publish_data):
         """Import the supplied path as a geo/alembic sop.
-        
+
         :param str path: The path to the file to import.
         :param dict sg_publish_data: The publish data for the supplied path.
-        
+
         """
 
         import hou
+
         app = self.parent
 
         name = sg_publish_data.get("name", "alembic")
@@ -211,8 +224,7 @@ class HoudiniActions(HookBaseClass):
         alembic_sop = geo_node.createNode("alembic", name)
         alembic_sop.parm("fileName").set(path)
         app.log_debug(
-            "Creating alembic sop: %s\n  path: '%s' " % 
-            (alembic_sop.path(), path)
+            "Creating alembic sop: %s\n  path: '%s' " % (alembic_sop.path(), path)
         )
         alembic_sop.parm("reload").pressButton()
 
@@ -228,6 +240,7 @@ class HoudiniActions(HookBaseClass):
         """
 
         import hou
+
         app = self.parent
 
         publish_name = sg_publish_data.get("name", "published_file")
@@ -236,10 +249,10 @@ class HoudiniActions(HookBaseClass):
         # remove non alphanumeric characers from the string (houdini node names
         # must be alphanumeric). first, build a regex to match non alpha-numeric
         # characters. Then use it to replace any matches with an underscore
-        
+
         # cannot use special characters to create nodes
-        pattern = re.compile('[\W_]+')
-        publish_name = pattern.sub('_', publish_name)
+        pattern = re.compile("[\W_]+")
+        publish_name = pattern.sub("_", publish_name)
 
         # get the publish path
         path = self.get_publish_path(sg_publish_data)
@@ -255,8 +268,7 @@ class HoudiniActions(HookBaseClass):
             # failed to create the node in the current context.
             img_context = hou.node("/img")
 
-            comps = [c for c in img_context.children()
-                if c.type().name() == "img"]
+            comps = [c for c in img_context.children() if c.type().name() == "img"]
 
             if comps:
                 # if there are comp networks, just pick the first one
@@ -277,11 +289,11 @@ class HoudiniActions(HookBaseClass):
             path = path.replace(full_frame_spec, "$F%s" % (padding,))
 
         file_cop.parm("filename1").set(path)
-        app.log_debug(
-            "Created file COP: %s\n  path: '%s' " % (file_cop.path(), path))
+        app.log_debug("Created file COP: %s\n  path: '%s' " % (file_cop.path(), path))
         file_cop.parm("reload").pressButton()
 
         _show_node(file_cop)
+
 
 ##############################################################################################################
 def _get_current_context(context_type):
@@ -303,7 +315,7 @@ def _get_current_context(context_type):
     network_tab = _get_current_network_panetab(context_type)
     if network_tab:
         context = network_tab.pwd()
-    
+
     return context
 
 
@@ -315,8 +327,9 @@ def _get_current_network_panetab(context_type):
         type. Example: "/obj"
 
     """
-    
+
     import hou
+
     network_tab = None
 
     # there doesn't seem to be a way to know the current context "type" since
@@ -325,9 +338,11 @@ def _get_current_network_panetab(context_type):
     # the specified context type that is the current tab in its pane. hopefully
     # that's the one the user is looking at.
     for panetab in hou.ui.paneTabs():
-        if (isinstance(panetab, hou.NetworkEditor) and
-            panetab.pwd().path().startswith(context_type) and
-            panetab.isCurrentTab()):
+        if (
+            isinstance(panetab, hou.NetworkEditor)
+            and panetab.pwd().path().startswith(context_type)
+            and panetab.isCurrentTab()
+        ):
 
             network_tab = panetab
             break
@@ -338,12 +353,12 @@ def _get_current_network_panetab(context_type):
 ##############################################################################################################
 def _show_node(node):
     """Frame the supplied node in the current network pane.
-    
+
     :param hou.Node node: The node to frame in the current network pane.
-    
+
     """
 
-    context_type = "/" + node.path().split("/")[0] 
+    context_type = "/" + node.path().split("/")[0]
     network_tab = _get_current_network_panetab(context_type)
 
     if not network_tab:
@@ -353,5 +368,3 @@ def _show_node(node):
     node.setSelected(True, clear_all_selected=True)
     network_tab.cd(node.parent().path())
     network_tab.frameSelection()
-
-    
