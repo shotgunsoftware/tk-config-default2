6ff1d714d17beb97504c4758123c534831088ecb
Darragh Duffy <darragh.duffy@vfx55.eggvfx.ie>
2022-01-12 18:05:23 +0000

    working on shots structure

---
+++ hooks/tk-multi-launchapp/before_app_launch.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+"""
+Before App Launch Hook
+
+This hook is executed prior to application launch and is useful if you need
+to set environment variables or run scripts as part of the app initialization.
+"""
+
+import os
+import tank
+import sgtk
+logger = sgtk.platform.get_logger(__name__)
+
+class BeforeAppLaunch(tank.Hook):
+    """
+    Hook to set up the system prior to app launch.
+    """
+
+    def execute(
+        self, app_path, app_args, version, engine_name, software_entity=None, **kwargs
+    ):
+        """
+        The execute function of the hook will be called prior to starting the required application
+
+        :param app_path: (str) The path of the application executable
+        :param app_args: (str) Any arguments the application may require
+        :param version: (str) version of the application being run if set in the
+            "versions" settings of the Launcher instance, otherwise None
+        :param engine_name (str) The name of the engine associated with the
+            software about to be launched.
+        :param software_entity: (dict) If set, this is the Software entity that is
+            associated with this launch command.
+        """
+
+        # accessing the current context (current shot, etc)
+        # can be done via the parent object
+        #
+        # > multi_launchapp = self.parent
+        # > current_entity = multi_launchapp.context.entity
+
+        # you can set environment variables like this:
+        # os.environ["MY_SETTING"] = "foo bar"
+        if engine_name == "tk-nuke":
+            # logger.debug("NUKE_PATH:{}".format(os.environ.get("NUKE_PATH")))
+            if os.environ.get("NUKE_PATH"):
+                logger.debug("Temp dir:{}".format(os.environ.get("NUKE_TEMP_DIR")))
+                logger.debug("NUKE_PATH already set")
+                logger.debug(os.environ.get("NUKE_PATH"))
+                tank.util.append_path_to_env_var("NUKE_PATH", "/mnt/pipeline/dcc/dev/nuke")
+                tank.util.append_path_to_env_var("NUKE_TEMP_DIR", "/tmp/nuke")
