5fea6bb0e71a622a1b3ff4fee69f6bc7087ce244
rthompson <rthompson@ssvfx.ie>
2022-07-06 16:33:40 +0100

    added outsource upload_version to submitter

---
+++ hooks/tk-multi-publish2/basic/outsource/outsource_upload_version.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2015 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+# ### OVERRIDDEN IN SSVFX_SG ###
+
+from ss_config.hooks.tk_multi_publish2.outsource.outsource_upload_version import SsOutsourceUploadVersionPlugin
+
+
+class UploadVersionPlugin(SsOutsourceUploadVersionPlugin):
+    """
+    Plugin for sending quicktimes and images to shotgun for review.
+    """
+    pass
