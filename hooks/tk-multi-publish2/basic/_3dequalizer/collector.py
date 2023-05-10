297efebee63f51471283661a0175cb98e5b442fc
exploreborders <mail@exploreborders.com>
2023-05-10 18:10:41 +0200

    3de

---
+++ hooks/tk-multi-publish2/basic/_3dequalizer/collector.py
@@ -0,0 +1,20 @@
+﻿# Copyright (c) 2017 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+
+from ss_config.hooks.tk_multi_publish2._3dequalizer.collector import Ss3dequalizerSessionCollector
+
+
+class _3dequalizerSessionCollector(Ss3dequalizerSessionCollector):
+    """
+    Collector that operates on the 3dequalizer session. Should inherit from the basic
+    collector hook.
+    """
+    pass
